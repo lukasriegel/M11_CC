682db0aeca6f5de601f4fdd00a25cff7e94f376b
Lukas Riegel <lukas.riegel@sap.com>
2016-12-10 05:37:22 -0800

    Bonus

--- serverBonus.js
+++ serverBonus.js
@@ -1,11 +1,17 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    response.end('process id: ' + process.pid);
-   
-}).listen(process.env.PORT);
+    if (request.method == 'POST') {
+        // Send the response body as "Hello Cloud"
+        response.end('Wow, this was an awesome POST request!\n');
+    }
+    else {
+        // Send the response body as "Hello Cloud"
+        response.end('Hello Cloud\n');
+    }
+}).listen(process.env.port);
